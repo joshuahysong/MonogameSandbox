89ba07bac8ad462fefa00a9735014a002fc7deb3
Joshua Hysong <jhysong@rhsinc.com>
2018-11-12 22:50:36 -0500

    Implemented newtonian flying

--- TestFlying/MainGame.cs
+++ TestFlying/MainGame.cs
@@ -14,10 +14,11 @@ namespace TestFlying
         SpriteBatch spriteBatch;
         Texture2D playerShip;
 
-        Vector2 velocity;
         Vector2 position;
-        float rotation;
-        int speed;
+        Vector2 velocity;
+        Vector2 acceleration;
+        float heading;
+        const float thrust = 100.0f;
 
         public MainGame()
         {
@@ -41,7 +42,6 @@ namespace TestFlying
             // TODO: Add your initialization logic here
 
             position = new Vector2(GraphicsDevice.Viewport.Bounds.Width / 2, GraphicsDevice.Viewport.Bounds.Height / 2);
-            speed = 0;
             base.Initialize();
         }
 
@@ -73,66 +73,35 @@ namespace TestFlying
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            KeyboardState keyboardState = Keyboard.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            //float velocity = 5f;
-
-            //double angle = 0;
-
-            //Vector2 trajectory = new Vector2(velocity) * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
-
-            //if (Keyboard.GetState().IsKeyDown(Keys.W))
-            //{
-            //    position += trajectory;
-            //}
-
-            //Vector2 direction = new Vector2((float)Math.Cos(rotationAngle), (float)Math.Sin(rotationAngle));
-            //direction.Normalize();
-            //position += direction * 2;
-
-            // Reset the velocity to zero after each update to prevent unwanted behavior
-            velocity = Vector2.Zero;
-
-            // Handle user input
             HandleInput(Keyboard.GetState(), gameTime);
 
             if (position.X <= 0)
             {
                 position.X = GraphicsDevice.Viewport.Bounds.Width;
             }
-
             if (position.X > GraphicsDevice.Viewport.Bounds.Width)
             {
                 position.X = 0;
             }
-
             if (position.Y <= 0)
             {
                 position.Y = GraphicsDevice.Viewport.Bounds.Height;
             }
-
             if (position.Y > GraphicsDevice.Viewport.Bounds.Height)
             {
                 position.Y = 0;
             }
-
-            Console.WriteLine(speed);
-
-            // Applies our speed to velocity
-            //velocity *= speed;
-
-            //velocity.X = (float)Math.Cos(rotation * 2 * Math.PI / 360);
-            //velocity.Y = (float)-Math.Sin(rotation * 2 * Math.PI / 360);
-
-            velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
-            velocity.Normalize();
+            
             // Seconds passed since iteration of update
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            // Multiplies our movement framerate independent by multiplying with deltaTime
-            position += (velocity * deltaTime) * speed;
+            position += velocity * deltaTime;
+            velocity += acceleration * deltaTime;
+            acceleration.X = 0;
+            acceleration.Y = 0;
 
             base.Update(gameTime);
         }
@@ -148,7 +117,7 @@ namespace TestFlying
             var textureCenter = new Vector2(playerShip.Width / 2, playerShip.Height / 2);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(playerShip, position, null, Color.White, rotation + (90 * (float)Math.PI / 180), textureCenter, 0.5f, SpriteEffects.None, 1f);
+            spriteBatch.Draw(playerShip, position, null, Color.White, heading + (90 * (float)Math.PI / 180), textureCenter, 0.5f, SpriteEffects.None, 1f);
             spriteBatch.End();
 
             base.Draw(gameTime);
@@ -158,29 +127,16 @@ namespace TestFlying
         {
             if (KeyState.IsKeyDown(Keys.W))
             {
-                //Speed up
-                speed += 1;
-                if (speed > 200)
-                {
-                    speed = 200;
-                }
+                acceleration.X += thrust * (float)Math.Cos(heading);
+                acceleration.Y += thrust * (float)Math.Sin(heading);
             }
-            if (KeyState.IsKeyDown(Keys.S))
-            {
-                speed -= 1;
-                if (speed < 0)
-                {
-                    speed = 0;
-                }
-            }
-
             if (KeyState.IsKeyDown(Keys.A))
             {
-                rotation -= 0.05F;
+                heading -= 0.05F;
             }
             if (KeyState.IsKeyDown(Keys.D))
             {
-                rotation += 0.05F;
+                heading += 0.05F;
             }
         }
     }
